5b2154cb9a53658ccaeb9a8b8afdd8be4c44df50
Ethan Illingsworth <illingsworth.ethan@gmail.com>
2025-09-27 09:50:20 -0500

    add Lokal to projects page and update projects to new system

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,14 +1,32 @@
-import $ from "jquery"
-import { Project } from "./main.js"
+import $ from "jquery";
+import { Project, Section, SubSection } from "./main.js";
 
+const projects = new Section("Projects");
+
+const active = new SubSection("Active");
+
+const activeRow = $("<div/>").addClass(
+	"grid grid-cols-1 md:grid-cols-3 grid-flow-row pt-4 gap-6"
+);
+
+active.addElement(activeRow);
+
+const archive = new SubSection("Archived");
+
+const archiveRow = $("<div/>").addClass(
+	"grid grid-cols-1 md:grid-cols-3 grid-flow-row pt-4 gap-6"
+);
+
+archive.addElement(archiveRow);
+
+projects.addSection(active);
+projects.addSection(archive);
 
 for (let project of Project.getAll()) {
-    console.log(project)
-    const data = project.get()
-    if (data.archived) {
-        project.display($("#archive-row"))
-    }
-    else {
-        project.display($("#projects-row"))
-    }
+	const data = project.get();
+	if (data.archived) {
+		project.display(archiveRow);
+	} else {
+		project.display(activeRow);
+	}
 }
